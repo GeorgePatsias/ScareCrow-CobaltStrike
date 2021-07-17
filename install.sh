c60dd402c58142cdf87cf2b711a34a8c0859ea55
UserX <george_patsias@hotmail.co.uk>
2021-07-17 13:31:10 +0300

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+sudo apt-get install openssl osslsigncode mingw-w64 -y;
+git clone https://github.com/optiv/ScareCrow;
+cd ScareCrow;
+go get github.com/fatih/color;
+go get github.com/yeka/zip;
+go get github.com/josephspurrier/goversioninfo;
+go build ScareCrow.go
+
+echo ""
+echo "[Success] All Done! Please configure the ScareCrow.cna variables now."
