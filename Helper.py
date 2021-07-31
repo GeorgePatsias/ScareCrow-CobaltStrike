677ce011039f8825e717a74f3384b92675f7feb9
UserX <george_patsias@hotmail.co.uk>
2021-07-31 12:27:55 +0300

    Update Helper.py

--- Helper.py
+++ Helper.py
@@ -4,28 +4,46 @@ from sys import argv
 from subprocess import check_output, Popen, PIPE, call
 
 
-def generate(executable, payload, loader, domain, cs_directory, etw, sandbox):
+def generate(executable, payload, loader, domain, cs_directory, etw, sandbox, loader_name):
 
     command = [executable, '-I', payload, '-Loader', loader, '-domain', domain]
 
-    if etw is "true":
+    if etw == 'true':
         command.append('-etw')
 
-    if sandbox is "true":
+    if sandbox == 'true':
         command.append('-sandbox')
+
+    if loader_name:
+        if not loader_name.endswith('.js'):
+            loader_name = "{}.js".format(loader_name)
+        command.append('-O')
+        command.append(loader_name)
+
+    filename = None
+    if loader in ["control", "excel", "msiexec", "wscript"] and loader_name:
+        filename = loader_name
+    elif loader in ["excel", "msiexec", "wscript"] and not loader_name:
+        filename = "Loader.js"
+        command.append('-O')
+        command.append(filename)
 
     process = Popen(command, stdout=PIPE)
     stdout, stderr = process.communicate()
 
-    reader = stdout.decode('utf-8').splitlines()
+    if loader in ['binary', "dll", "control"]:
+        for row in stdout.decode('utf-8').splitlines():
+            if row.startswith('[*] Signing'):
+                filename = row
+                break
 
-    filename = None
-    for row in reader:
-        if row.startswith('[*] Signing'):
-            filename = row
-            break
+        filename = filename.split('[*] Signing ')[1].split(' With a Fake Cert')[0]
 
-    filename = filename.split('[*] Signing ')[1].split(' With a Fake Cert')[0]
+        if loader == "control" and not loader_name:
+            filename = filename[:-3] + "cpl"
+
+        elif loader == "control" and loader_name:
+            filename = loader_name
 
     call(['mv', cs_directory + '/' + filename, os.path.dirname(payload) + "/"])
     check_output(['rm', '-f', payload])
@@ -36,5 +54,8 @@ def generate(executable, payload, loader, domain, cs_directory, etw, sandbox):
 if __name__ == '__main__':
     arg_list = argv[1:]
 
-    shellcode_dir = generate(arg_list[0], arg_list[1], arg_list[2], arg_list[3], arg_list[4], arg_list[5], arg_list[6])
+    if len(arg_list) < 8:
+        arg_list.append(None)
+
+    shellcode_dir = generate(arg_list[0], arg_list[1], arg_list[2], arg_list[3], arg_list[4], arg_list[5], arg_list[6], arg_list[7])
     print(shellcode_dir)
